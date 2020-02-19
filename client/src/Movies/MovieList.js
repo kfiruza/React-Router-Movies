e44d6cf7f066bdd8d4a1c753d747f744071e88f7
khashimovafiruza@gmail.com <khashimovafiruza@gmail.com>
2020-02-18 22:35:12 -0600

    fixed some issues

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,22 +1,22 @@
 import React, { useState, useEffect } from 'react';
-import MovieCard from "./MovieCard";
+import { Link } from 'react-router-dom';
 import axios from 'axios';
+import MovieCard from './MovieCard';
 
 const MovieList = props => {
   const [movies, setMovies] = useState([])
-  
   useEffect(() => {
     const getMovies = () => {
       axios
         .get('http://localhost:5000/api/movies')
         .then(response => {
           setMovies(response.data);
-          console.log(response.data)          
         })
         .catch(error => {
           console.error('Server Error', error);
         });
-    }    
+    }
+    
     getMovies();
   }, []);
   
@@ -24,8 +24,9 @@ const MovieList = props => {
     <div className="movie-list">
       {movies.map(movie => (
         
-        <MovieCard key={movie.id} movie={movie} />
-        
+        <Link to={`/movies/${movie.id}`} key={movie.id}>
+          <MovieCard key={movie.id} movie={movie} />
+        </Link>
       ))}
     </div>
   );
